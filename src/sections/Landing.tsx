08a2ed52e0efcea53f85a0684ba700f446f9215e
RjSup <19141558@brookes.ac.uk>
2025-07-22 17:36:34 +0100

    ---

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -3,7 +3,7 @@ export default function Landing() {
     <div id="landing" className="w-full h-screen flex flex-col justify-start items-center p-6 snap-start">
       <h1 className="text-3xl font-bold text-black pb-92 pt-24">Ryan James</h1>
       <h2 className="text-xl text-black mt-2">
-        Aspiring Software Developer
+        Aspiring Software Developer.
       </h2>
       <p className="text-black mt-2">Building intelligent, interactive, and impactful software.</p>
     </div>
